cae629f23da80759eae284fe55ff339f8c3f1b41
Serhgag <gullschii421@gmail.com>
2023-03-20 14:07:48 +0700

    Add Eisting project files to Git

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,15 @@
+<?php
+    class Contoh1 extends CI_Controller
+  { 
+
+	    public function index()
+	  {
+        
+      echo "<h1>Perkenalkan</h1>";
+      echo"Nama saya Tangguh Rivaldi mahasiswa ubsi semesester6
+      Saya tingga di daerah Depok
+      olah raga yang saya sukai adalah
+      Futsal & Sepakbola";
+    }
+  }
+?>
